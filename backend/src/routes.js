9beb7948890f48a9253b9ac0c9a16f695eda9752
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-11 19:29:39 -0300

    create route of register

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const routes = express.Router();
+const userMiddlewares = require("./middlewares/userMiddlewares");
+const userController = require("./controller/userController");
 
-routes.post("/register");
+routes.post(
+  "/register",
+  userMiddlewares.validateRegister,
+  userController.createUser
+);
 routes.post("/login");
 
 module.exports = routes;
