581b2384fad5e118b8c231071cfc9e1adc6ff2f6
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-12 22:29:14 -0300

    creating function to get user by email

--- backend/src/modules/userModel.js
+++ backend/src/modules/userModel.js
@@ -1,12 +1,24 @@
 const connection = require("./connection");
 
 const createUser = async (user) => {
-  const query = "INSERT INTO users VALUES(null,?,?,?)";
   const { name, email, password } = user;
-  const [res] = await connection.execute(query, [name, email, password]);
-  return res.insertId;
+  const queryInsert = "INSERT INTO users VALUES(null,?,?,?)";
+
+  const [resInsert] = await connection.execute(queryInsert, [
+    name,
+    email,
+    password,
+  ]);
+  return { insertId: resInsert.insertId };
+};
+
+const getUser = async (user) => {
+  const querySelect = "SELECT * FROM users WHERE email=?";
+  const [resSelect] = await connection.execute(querySelect, [user.email]);
+  return resSelect;
 };
 
 module.exports = {
   createUser,
+  getUser,
 };
