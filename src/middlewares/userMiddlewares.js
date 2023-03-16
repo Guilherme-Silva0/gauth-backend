3afc1fc262c9bdc12fb2058bdd0ed2caba6f1735
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-16 20:53:53 -0300

    creating login validations

--- src/middlewares/userMiddlewares.js
+++ src/middlewares/userMiddlewares.js
@@ -47,7 +47,33 @@ const validateCode = async (req, res, next) => {
   next();
 };
 
+const validateLogin = async (req, res, next) => {
+  const schema = yup.object().shape({
+    email: yup
+      .string("email has to be of type string")
+      .email("email invalid")
+      .required("email is required!")
+      .trim(),
+    password: yup
+      .string("password has to be of type string")
+      .min(6, "the password must have at least 6 characters")
+      .required("password is required!")
+      .trim(),
+  });
+
+  try {
+    await schema.validate(req.body);
+    next();
+  } catch (err) {
+    return res.status(400).json({
+      error: true,
+      message: err.errors,
+    });
+  }
+};
+
 module.exports = {
   validateRegister,
   validateCode,
+  validateLogin,
 };
