2fd62c45da72b32a84b757d4b7bd11ba48f8b91d
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-17 14:24:38 -0300

    limiting access to the api

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", optionsSuccessStatus: 200 }));
 app.use(routes);
 
 app.listen(PORT, () => console.log("server is running in port", PORT));
